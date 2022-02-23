742c4ca9cb309af03da55d2eb1d7572d29ca9f56
oliviabudweth <olivia.budweth@icloud.com>
2022-02-23 23:49:25 +0000

    W6D2

--- app/actions.rb
+++ app/actions.rb
@@ -88,9 +88,15 @@ end
 
   post '/likes' do
     finstagram_post_id = params[:finstagram_post_id]
-
+  
     like = Like.new({ finstagram_post_id: finstagram_post_id, user_id: current_user.id })
     like.save
   
+    redirect(back)
+  end
+
+  delete '/likes/:id' do
+    like = Like.find(params[:id])
+    like.destroy
     redirect(back)
   end
